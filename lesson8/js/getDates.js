e51ed4139da2df35df042c329c4508883bbf851b
Na Rah Kwak <kwa18001@byui.edu>
2021-11-07 21:50:15 -0700

    week8

---
+++ lesson8/js/getDates.js
@@ -0,0 +1,9 @@
+const hambutton = document.querySelector(".hamburger");
+const mainnav = document.querySelector(".navigation");
+
+hambutton.addEventListener("click", () => {mainnav.classList.toggle("responsive")}, false);
+
+window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+
+/*Footer copyright date */
+document.querySelector("#copyright").textContent = new Date().getFullYear();
