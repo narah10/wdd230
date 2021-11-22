2911e8037118a6a532974b02659ea0b3df721321
Na Rah Kwak <kwa18001@byui.edu>
2021-11-21 19:36:20 -0700

    week6

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -2,8 +2,8 @@ let windchill = parseFloat(document.querySelector("#wind-value").textContent);
 let temp = parseFloat(document.querySelector("#temp").textContent);
 let speed = parseFloat(document.querySelector("#speed").textContent);
 
-if (temp <= 50 && speed > 4.8) {
-    let f = (35.74 + (0.6215 * temp) - (35.75 * (Math.pow(speed, 0.16))) + (0.4275 * temp * (Math.pow(speed, 0.16)))).toFixed(2);
+if (temp <= 50 && speed > 3) {
+    let f = (35.74 + (0.6215 * temp) - (35.75 * (Math.pow(speed, 0.16))) + (0.4275 * temp * (Math.pow(speed, 0.16)))).toFixed(0);
     document.querySelector("#wind-value").innerHTML = `${f}&#176;F`;
 } else{
     document.querySelector("#wind-value").innerHTML = "N/A";
